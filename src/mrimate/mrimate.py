fix error in phase and velocity calculation

src/mrimate/mrimate.py
@@ -96,7 +96,7 @@ class MRImateExperiment:
             self.data = np.reshape(self.data, target_shape)
             self.spin_density =  self.data[:, :, :, 0::2]#only for 1d velocity encoded
             phase_integer =  self.data[:, :, :, 1::2]#only for 1d velocity encoded
-            self.phase = (phase_integer-np.max(phase_integer)/2)/np.max(phase_integer)/2*np.pi
+            self.phase = (phase_integer-4096/2)/(4096/2)*np.pi
 
         elif self.parameters.MaxNumberOfEchoes > 1:
             target_shape = (self.data.shape[0],self.data.shape[1], self.parameters.NumberOfSlices, self.parameters.MaxNumberOfEchoes)
@@ -205,7 +205,7 @@ class MRImateExperiment:
         if not self.data_loaded:
             raise ValueError("Data not loaded. Call 'load()' method first.")
         if self.parameters.is_flow_encoded():
-            self.velocity = self.phase*self.parameters.MaxEncodedVelocity #so far only valid for 1d velocity
+            self.velocity = self.phase*self.parameters.MaxEncodedVelocity/np.pi #so far only valid for 1d velocity
         else:
             raise ValueError("Phase data is not available. Ensure velocity encoding is present.")
 